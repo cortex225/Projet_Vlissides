012c80a723fc7c32ebeb60adacbc3444c0c7144b
jlgouaho <1900751@CegepGranby.qc.ca>
2023-08-24 15:56:24 -0400

    Adding Langue
    plus liaisons à livre

---
+++ VLISSIDES/VLISSIDES/Models/Langue.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace VLISSIDES.Models;
+
+public class Langue
+{
+    
+    public string Id { get; set; }
+    
+    public string Nom { get; set; }
+    
+    public string Code { get; set; }
+    
+    public string Description { get; set; }
+
+}
