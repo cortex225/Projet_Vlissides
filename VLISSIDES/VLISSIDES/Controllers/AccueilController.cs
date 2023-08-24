61720b83d4fb61aa1c16d9a2bd02bb7ddb950b06
jlgouaho <1900751@CegepGranby.qc.ca>
2023-08-24 13:02:42 -0400

    Modification de Home par Accueil

--- VLISSIDES/VLISSIDES/Controllers/AccueilController.cs
+++ VLISSIDES/VLISSIDES/Controllers/AccueilController.cs
@@ -4,11 +4,11 @@ using VLISSIDES.Models;
 
 namespace VLISSIDES.Controllers;
 
-public class HomeController : Controller
+public class AccueilController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly ILogger<AccueilController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public AccueilController(ILogger<AccueilController> logger)
     {
         _logger = logger;
     }
--- VLISSIDES/VLISSIDES/Program.cs
+++ VLISSIDES/VLISSIDES/Program.cs
@@ -23,7 +23,7 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Accueil/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -38,7 +38,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     "default",
-    "{controller=Home}/{action=Index}/{id?}");
+    "{controller=Accueil}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
