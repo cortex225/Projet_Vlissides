8e1d50510841ad6470361aa3abe8f91b1686c538
jlgouaho <1900751@CegepGranby.qc.ca>
2023-08-24 15:46:22 -0400

    Ajout Reservation, Evenement, updating Membre

---
+++ VLISSIDES/VLISSIDES/Models/Evenement.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace VLISSIDES.Models;
+
+public class Evenement
+{
+    public string Id { get; set; }
+    
+    public string Nom { get; set; }
+    
+    public string Description { get; set; }
+    
+    public DateTime DateDebut { get; set; }
+    
+    public DateTime DateFin { get; set; }
+    
+    public ICollection<Reservation> Reservations { get; set; }
+}
