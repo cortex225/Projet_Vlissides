725f9189c6fb83d3ad74dd9b11f8cb1e0f7734c8
David-Messier <99209339+David-Messier@users.noreply.github.com>
2023-08-24 10:17:56 -0400

    Remplacer la page d'acceuille de Home/Index à Home/Acceuille

--- VLISSIDES/VLISSIDES/Program.cs
+++ VLISSIDES/VLISSIDES/Program.cs
@@ -38,7 +38,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Acceuille}/{id?}");
 app.MapRazorPages();
 
 app.Run();
