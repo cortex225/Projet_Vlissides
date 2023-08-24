01e93a5902d1c72e17f393a8d3b2eae704ba3281
jlgouaho <1900751@CegepGranby.qc.ca>
2023-08-24 16:05:28 -0400

    Ajout DBSet
    et modification pour les pages Identity

--- VLISSIDES/VLISSIDES/Data/ApplicationDbContext.cs
+++ VLISSIDES/VLISSIDES/Data/ApplicationDbContext.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using VLISSIDES.Models;
 
 namespace VLISSIDES.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
     }
+    
+    public DbSet<Adresse> Adresses { get; set; }
+    
+    public DbSet<Commande> Commandes { get; set; }
+    
+    public DbSet<Employe> Employes { get; set; }    
+    
+    public DbSet<Membre> Membres { get; set; }
+    
+    public DbSet<Reservation> Reservations { get; set; }
+    
 }
