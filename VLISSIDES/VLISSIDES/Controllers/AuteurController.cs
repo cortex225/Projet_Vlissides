f3ef6dc8decfef00d03706c40818723b72447f02
David-Messier <99209339+David-Messier@users.noreply.github.com>
2023-08-24 10:45:23 -0400

    Ajouter controller + rapide sintaxe standardisation

---
+++ VLISSIDES/VLISSIDES/Controllers/AuteurController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VLISSIDES.Viewmodels;
+
+namespace VLISSIDES.Controllers
+{
+    public class AuteurController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new IndexAuteurViewModel());
+        }
+        public IActionResult Ajouter()
+        {
+            return View(new AjouterAuteurViewModel());
+        }
+        public IActionResult Retirer()
+        {
+            return View(new RetirerAuteurViewModel());
+        }
+    }
+}
