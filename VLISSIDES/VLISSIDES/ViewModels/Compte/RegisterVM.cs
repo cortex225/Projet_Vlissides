4bcf75d77bafa593942e37a568253a8c728d8d38
jlgouaho <1900751@CegepGranby.qc.ca>
2023-08-31 01:17:07 -0400

    Register/Login
    Ajout de toutes les class, VM et nuget necessaires au bon fonctionnement de cette fonctionnalité.
    Aussi configuration du systeme de courriel

---
+++ VLISSIDES/VLISSIDES/ViewModels/Compte/RegisterVM.cs
@@ -0,0 +1,51 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace VLISSIDES.ViewModels.Compte;
+
+public class RegisterVM
+{
+    [Display(Name = "Nom d'utilisateur")] public string? UserName { get; set; }
+
+    [Required(ErrorMessage = "Le prenom est requis.")]
+    [Display(Name = "Prenom")]
+    public string FirstName { get; set; } = default!;
+
+    [Required(ErrorMessage = "Le Nom est requis.")]
+    [Display(Name = "Nom")]
+    public string LastName { get; set; } = default!;
+
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email")]
+
+    public string Email { get; set; } = default!;
+
+    [Required(ErrorMessage = "Le telephone est requis.")]
+    [Phone]
+    [Display(Name = "Téléphone")]
+    public string Phone { get; set; } = default!;
+
+
+    [Required]
+    [DataType(DataType.Password)]
+    [StringLength(100, ErrorMessage = "Le {0} doit contenir au moins {2} et au maximum {1} caractères.",
+        MinimumLength = 6)]
+    [Display(Name = "Mot de passe")]
+    public string Password { get; set; } = default!;
+
+    [DataType(DataType.Password)]
+    [Required(ErrorMessage = "Veuillez confirmer votre mot de passe")]
+    [Display(Name = "Confirmation mot de passe")]
+    [Compare("Password", ErrorMessage = "Le mot de passe et la confirmation ne correspondent pas.")]
+    public string ConfirmPassword { get; set; } = default!;
+
+    [Display(Name = "Conditions d'utilisation")]
+    [Required(ErrorMessage = "Veuillez accepter les conditions d'utilisation")]
+    public bool Conditions { get; set; }
+
+    public string? ReturnUrl { get; set; }
+    public string? CompanyName { get; set; }
+
+
+}
