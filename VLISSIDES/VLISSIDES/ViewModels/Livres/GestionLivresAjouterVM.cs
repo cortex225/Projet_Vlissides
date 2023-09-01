d6163c1243f4505c2a118bcc64e05c92554cb152
TonyTheProg <tranloan946@gmail.com>
2023-09-01 10:54:08 -0400

    Création du ViewModel GestionLivresAjouterVM.cs

---
+++ VLISSIDES/VLISSIDES/ViewModels/Livres/GestionLivresAjouterVM.cs
@@ -0,0 +1,24 @@
+﻿namespace VLISSIDES.ViewModels.Livres
+{
+    public class GestionLivresAjouterVM
+    {
+        public string Titre { get; set; }
+        public string Resume { get; set; }
+        public string Couverture { get; set; }
+        public int NbExemplaires { get; set; }
+        public int NbPages { get; set; }
+        public double Prix { get; set; }
+        public DateTime DatePublication { get; set; }
+
+        public string ISBN { get; set; }
+
+        public string CategorieId { get; set; }
+        public string AuteurId { get; set; }
+        public string MaisonEditionId { get; set; }
+
+        public string TypeLivreId { get; set; }
+        public string EvaluationId { get; set; }
+        public string LangueId { get; set; }
+
+    }
+}
