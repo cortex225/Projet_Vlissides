b909b6cf7fcaede22d515dd9890e63bc2a40e42b
Etienne225 <2167594@CegepGranby.qc.ca>
2023-09-06 17:35:46 -0400

    Ajouté code pour seeder mais ne fonctionne pas car bug au niveau de la base de donnés

--- VLISSIDES/Seeder/Program.cs
+++ VLISSIDES/Seeder/Program.cs
@@ -1,3 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using FizzWare.NBuilder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Logging;
+using Seeder;
+using VLISSIDES.Models;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Début du seed!");
+using var context = DbContextFactory.CreateDbContext();
+
+var categories = context.Categories.ToList();
+var typeLivres = context.TypeLivres.ToList();
+var evaluations = context.Evaluations.ToList();
+var langues = context.Langues.ToList();
+
+var generator = new RandomGenerator();
+
+var auteurs = Builder<Auteur>.CreateListOfSize(99)
+    .All()
+    .With(c => c.Nom = Faker.Name.Last())
+    .With(c => c.Prenom = Faker.Name.First())
+    .With(c => c.Biographie = Faker.Lorem.Paragraph())
+    .With(c => c.Photo = "~/jean-luc.png")
+    .Build();
+context.Auteurs.AddRange(auteurs); context.SaveChanges();
+
+var maisonsEditions = Builder<MaisonEdition>.CreateListOfSize(99)
+    .All()
+    .With(c => c.Nom = Faker.Company.Name())
+    .Build();
+context.MaisonEditions.AddRange(maisonsEditions); context.SaveChanges();
+
+var livres = Builder<Livre>.CreateListOfSize(2999)
+    .All()
+    .With(c => c.Titre = Faker.Company.Name())
+    .With(c => c.Resume = Faker.Lorem.Paragraph())
+    .With(c => c.Couverture = "https://i.pinimg.com/236x/37/a9/98/37a99839a447357ee6d3d4b9c991d864.jpg")
+    .With(c => c.NbExemplaires = 1)
+    .With(c => c.DateAjout = DateTime.Now)
+    .With(c => c.NbPages = 120)
+    .With(c => c.Prix = 1.99)
+    .With(c => c.DatePublication = Faker.Identification.DateOfBirth())
+    .With(c => c.ISBN = Faker.Identification.UsPassportNumber())
+    .With(c => c.Categories = (ICollection<Categorie>)Pick<Categorie>.RandomItemFrom(categories))
+    .With(c => c.Auteur = (ICollection<Auteur>)Pick<Auteur>.RandomItemFrom(auteurs))
+    .With(c => c.MaisonEdition = Pick<MaisonEdition>.RandomItemFrom(maisonsEditions))
+    .With(c => c.TypesLivre = (ICollection<TypeLivre>)Pick<TypeLivre>.RandomItemFrom(typeLivres))
+    .With(c => c.Evaluations = (ICollection<Evaluation>)Pick<Evaluation>.RandomItemFrom(evaluations))
+    .With(c => c.Langues = (ICollection<Langue>)Pick<Langue>.RandomItemFrom(langues))
+    .Build();
+context.Livres.AddRange(livres); context.SaveChanges();
+
+Console.WriteLine("Fin du seed!");
