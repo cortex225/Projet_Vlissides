918ca5f72925c271337db812864c195142ae0f3a
Etienne225 <2167594@CegepGranby.qc.ca>
2023-09-11 09:08:50 -0400

    Création des display name pour ViewModels et Models

--- VLISSIDES/VLISSIDES/Models/Membre.cs
+++ VLISSIDES/VLISSIDES/Models/Membre.cs
@@ -1,18 +1,27 @@
+using System.ComponentModel;
+
 namespace VLISSIDES.Models;
 
 public class Membre : ApplicationUser
 {
+    [DisplayName("Numéro de membre")]
     public string NoMembre { get; set; } = default!;
 
+    [DisplayName("Date d'adhésion")]
     public DateTime DateAdhesion { get; set; } = default!;
 
+    [DisplayName("Identifiant de la commande")]
     public string? CommandeId { get; set; }
 
+    [DisplayName("Commandes")]
     public ICollection<Commande>? Commandes { get; set; }
 
+    [DisplayName("Identifiant de la réservation")]
     public string? ReservationId { get; set; } = default!;
 
+    [DisplayName("Réservations")]
     public ICollection<Reservation>? Reservations { get; set; }
 
+    [DisplayName("Livres favoris")]
     public ICollection<Favori>? Favoris { get; set; }
 }
