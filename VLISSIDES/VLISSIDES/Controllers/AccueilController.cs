8a41d672801e63a7272efd548aecb6d0162e4d2f
David-Messier <99209339+David-Messier@users.noreply.github.com>
2023-09-11 01:41:37 -0400

    Modifie accueil pour qu'il soit dynamic. Conglomérer le css d'accueil dans site.css pour l'instant

--- VLISSIDES/VLISSIDES/Controllers/AccueilController.cs
+++ VLISSIDES/VLISSIDES/Controllers/AccueilController.cs
@@ -1,21 +1,31 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using VLISSIDES.Data;
 using VLISSIDES.ViewModels;
+using VLISSIDES.ViewModels.Accueil;
 
 namespace VLISSIDES.Controllers;
 
 public class AccueilController : Controller
 {
+    private readonly ApplicationDbContext _context;
     private readonly ILogger<AccueilController> _logger;
 
-    public AccueilController(ILogger<AccueilController> logger)
+    public AccueilController(ApplicationDbContext context, ILogger<AccueilController> logger)
     {
+        _context = context;
         _logger = logger;
     }
 
     public IActionResult Index()
     {
-        return View();
+        List<_ServiceCardVM> services = new() { new("img/jean-luc.png", "Perfection", "♥"), new(), new(), new(), new() };
+        List<_EventCardVM> evenements = new() { new("img/logo/Logo.png", "Existance", "♦"), new(), new(), new(), new() };
+        List<_LivreCardVM> vedettes = new() { new("img/flat.png", "Titre", 6.66, new() { new() { Nom = "a", Prenom = "b" } }, new() { new() { Nom = "♣" } }), new(), new(), new(), new() };
+        List<_LivreCardVM> recommandations = new() { new("img/flat.png", "Ta vie", 0.69, new() { new() { Nom = "ka", Prenom = "a" } }, new() { new() { Nom = "♠" } }), new(), new(), new(), new() };
+        List<string> categories = new() { "1", "2", "3", "4", "5", };
+        List<_LivreCardVM> livreCategories = new() { new("img/flat.png", "۞", 0.69, new() { new() { Nom = "123", Prenom = "654" } }, new() { new() { Nom = "2" } }), new("img/flat.png", "۝", 0.69, new() { new() { Nom = "ڣ", Prenom = "ٻٸٷ" } }, new() { new() { Nom = "2" } }), new(), new(), new() };
+        return View(new IndexAccueilVM(services, evenements, vedettes, recommandations, categories, livreCategories));
     }
 
     public IActionResult Privacy()
---
+++ VLISSIDES/VLISSIDES/ViewModels/Accueil/IndexAccueilVM.cs
@@ -0,0 +1,24 @@
+﻿namespace VLISSIDES.ViewModels.Accueil
+{
+    public class IndexAccueilVM
+    {
+        public List<_ServiceCardVM> Services { get; set; }
+        public List<_EventCardVM> Evenements { get; set; }
+        public List<_LivreCardVM> Vedettes { get; set; }
+        public List<_LivreCardVM> Recommendations { get; set; }
+        public List<string> Categories { get; set; }
+        public List<_LivreCardVM> LivreCategories { get; set; }
+
+        public IndexAccueilVM(List<_ServiceCardVM> services = default!, List<_EventCardVM> evenement = default!,
+            List<_LivreCardVM> vedettes = default!, List<_LivreCardVM> recommendations = default!,
+            List<string> categories = default!, List<_LivreCardVM> livreCatégories = default!)
+        {
+            Services = services;
+            Evenements = evenement;
+            Vedettes = vedettes;
+            Recommendations = recommendations;
+            Categories = categories;
+            LivreCategories = livreCatégories;
+        }
+    }
+}
