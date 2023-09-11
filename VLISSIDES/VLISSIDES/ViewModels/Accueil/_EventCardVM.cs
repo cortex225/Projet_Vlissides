51b06fa94fb4364b92a67b67291bfd80e4eb0547
David-Messier <99209339+David-Messier@users.noreply.github.com>
2023-09-11 01:41:37 -0400

    Modifie accueil pour qu'il soit dynamic. Conglomérer le css d'accueil dans site.css pour l'instant

--- VLISSIDES/VLISSIDES/ViewModels/Accueil/_EventCardVM.cs
+++ VLISSIDES/VLISSIDES/ViewModels/Accueil/_EventCardVM.cs
@@ -2,9 +2,25 @@
 
 using VLISSIDES.Models;
 
-namespace VLISSIDES.ViewModels.Accueil;
-
-public class _EventCardVM
+namespace VLISSIDES.ViewModels.Accueil
 {
-    public Evenement monEvenement = new();
+    public class _EventCardVM
+    {
+        public string Image { get; set; }
+        public string Nom { get; set; }
+        public string Description { get; set; }
+
+        public _EventCardVM(string image = "", string nom = "", string description = "")
+        {
+            Image = image;
+            Nom = nom;
+            Description = description;
+        }
+        public _EventCardVM(Evenement evenement)
+        {
+            Image = evenement.Image;
+            Nom = evenement.Nom;
+            Description = evenement.Description;
+        }
+    }
 }
--- VLISSIDES/VLISSIDES/ViewModels/Accueil/_LivreCardVM.cs
+++ VLISSIDES/VLISSIDES/ViewModels/Accueil/_LivreCardVM.cs
@@ -2,9 +2,48 @@
 
 using VLISSIDES.Models;
 
-namespace VLISSIDES.ViewModels.Accueil;
-
-public class _LivreCardVM
+namespace VLISSIDES.ViewModels.Accueil
 {
-    public Livre monLivre { get; set; } = new();
+    public class _LivreCardVM
+    {
+        public string Couverture { get; set; }
+        public string Titre { get; set; }
+        public double Prix { get; set; }
+        public string Auteurs { get; set; }
+        public int Score { get; set; }
+        public List<string> Categories { get; set; }
+
+        public _LivreCardVM(string couverture = "", string titre = "", double prix = 0, List<Auteur> auteurs = default!, List<Categorie> categories = default!)
+        {
+            auteurs ??= new();
+            categories ??= new();
+            Couverture = couverture;
+            Titre = titre;
+            Prix = prix;
+            Auteurs = "";
+            if (auteurs.Any())
+            {
+                auteurs.ForEach(auteur => Auteurs += auteur.NomComplet + ", ");
+                Auteurs.Remove(Auteurs.Length - 2, 2);
+            }
+            Categories ??= new();
+            categories.ForEach(categorie => Categories.Add(categorie.Nom));
+        }
+        public _LivreCardVM(Livre livre)
+        {
+            livre.Evaluations ??= new List<Evaluation>();
+            Couverture = livre.Couverture;
+            Titre = livre.Titre;
+            Prix = livre.Prix;
+            Auteurs = "";
+            Score = (int)livre.Evaluations.Select(evaluation => evaluation.Note).Average();
+            if (!((List<Auteur>)livre.Auteur).Any())
+            {
+                ((List<Auteur>)livre.Auteur).ForEach(auteur => Auteurs += auteur.NomComplet + ", ");
+                Auteurs.Remove(Auteurs.Length - 2, 2);
+            }
+            Categories ??= new();
+            ((List<Auteur>)livre.Categories).ForEach(categorie => Categories.Add(categorie.Nom));
+        }
+    }
 }
