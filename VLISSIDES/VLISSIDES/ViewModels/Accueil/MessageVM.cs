642d5f19f315d37e74280e654cd98fff7490ddc5
David-Messier <99209339+David-Messier@users.noreply.github.com>
2023-09-13 17:48:28 -0400

    Création de view Message.

---
+++ VLISSIDES/VLISSIDES/ViewModels/Accueil/MessageVM.cs
@@ -0,0 +1,14 @@
+﻿namespace VLISSIDES.ViewModels.Accueil
+{
+    public class MessageVM
+    {
+        public string Tittre { get; set; }
+        public string Message { get; set; }
+
+        public MessageVM(string tittre, string message)
+        {
+            Tittre = tittre;
+            Message = message;
+        }
+    }
+}
