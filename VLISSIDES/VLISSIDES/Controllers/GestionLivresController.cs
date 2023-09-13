744e18a30a419b02f1e6a72a9b0bb0dcddc983a3
TonyTheProg <tranloan946@gmail.com>
2023-09-13 14:55:11 -0400

    img par défaut dans site deployé

--- VLISSIDES/VLISSIDES/Controllers/GestionLivresController.cs
+++ VLISSIDES/VLISSIDES/Controllers/GestionLivresController.cs
@@ -104,7 +104,7 @@ public class GestionLivresController : Controller
             }
             else
             {
-                vm.CoverImageUrl = $"{_config.GetValue<string>("ImageUrl")}livredefault.png";
+                vm.CoverImageUrl = "/2172811/img/CouvertureLivre/livredefault.png";
             }
             //Types de livres
             List<TypeLivre> listeType = new List<TypeLivre>();
