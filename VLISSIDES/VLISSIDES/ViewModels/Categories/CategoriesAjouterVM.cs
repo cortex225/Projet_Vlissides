91b3140a9b3383eeb5e47f3c7745646f6a98bf36
TonyTheProg <tranloan946@gmail.com>
2023-09-21 13:23:47 -0400

    Les VM

---
+++ VLISSIDES/VLISSIDES/ViewModels/Categories/CategoriesAjouterVM.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace VLISSIDES.ViewModels.Categories
+{
+    public class CategoriesAjouterVM
+    {
+        public string Nom { get; set; }
+        public string Description { get; set; }
+        public List<SelectListItem>? CategoriesParents { get; set; }
+        public string? ParentId { get; set; }
+    }
+}
---
+++ VLISSIDES/VLISSIDES/ViewModels/Categories/CategoriesModifierVM.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace VLISSIDES.ViewModels.Categories
+{
+    public class CategoriesModifierVM
+    {
+        public string Id { get; set; }
+        public string Nom { get; set; }
+        public string Description { get; set; }
+        public string? ParentId { get; set; }
+        public List<SelectListItem>? CategoriesParents { get; set; }
+    }
+}
