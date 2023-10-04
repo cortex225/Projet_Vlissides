b7ac8a1e99b8afb1ad5ad7cbbfc6ddcbb3d8e06f
BoomBoxProg <118985204+BoomBoxProg@users.noreply.github.com>
2023-10-04 16:24:56 -0400

    Afficher panier design

---
+++ VLISSIDES/VLISSIDES/Controllers/PanierController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VLISSIDES.Controllers
+{
+    public class PanierController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
