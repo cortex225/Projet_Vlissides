8997a77b345460432979c3a91fa8723bd48a1c7f
TonyTheProg <tranloan946@gmail.com>
2023-10-05 08:35:46 -0400

    icon information de base et VM adresses

--- VLISSIDES/VLISSIDES/ViewModels/Profile/ProfileModifierAdressesVM.cs
+++ VLISSIDES/VLISSIDES/ViewModels/Profile/ProfileModifierAdressesVM.cs
@@ -1,6 +1,10 @@
-﻿namespace VLISSIDES.ViewModels.Profile
+﻿using VLISSIDES.Models;
+
+namespace VLISSIDES.ViewModels.Profile
 {
     public class ProfileModifierAdressesVM
     {
+        public string AdressePrincipale { get; set; }
+        public List<Adresse>? AdressesDeLivraison { get; set; }
     }
 }
