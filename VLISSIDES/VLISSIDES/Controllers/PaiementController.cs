8e0871d784167e7b7e24907b5263fd06e0acbeda
BoomBoxProg <118985204+BoomBoxProg@users.noreply.github.com>
2023-10-23 11:29:28 -0400

    Display stepper

---
+++ VLISSIDES/VLISSIDES/Controllers/PaiementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VLISSIDES.Controllers
+{
+    public class PaiementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
