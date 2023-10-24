5efc5c5386a52c7ad8cb2d56a6b9219a1a441062
TonyTheProg <tranloan946@gmail.com>
2023-10-24 13:16:16 -0400

    Supprimer un compte, fonctionnel

--- VLISSIDES/VLISSIDES/Controllers/GestionComptesController.cs
+++ VLISSIDES/VLISSIDES/Controllers/GestionComptesController.cs
@@ -13,13 +13,15 @@ namespace VLISSIDES.Controllers
         private readonly ApplicationDbContext _context;
         private readonly IWebHostEnvironment _webHostEnvironment;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
         public GestionComptesController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment,
-        IConfiguration config, UserManager<ApplicationUser> userManager)
+        IConfiguration config, UserManager<ApplicationUser> userManager, IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
             _webHostEnvironment = webHostEnvironment;
             _config = config;
             _userManager = userManager;
+            _httpContextAccessor = httpContextAccessor;
         }
         public IActionResult Index()
         {
@@ -69,6 +71,49 @@ namespace VLISSIDES.Controllers
                 Courriel = u.Email
             }).ToList();
             return PartialView("PartialViews/GestionComptes/_ListeAdminPartial", Admin);
+        }
+        #region Confirmation de supprimer
+        public IActionResult ShowDeleteConfirmationMembre(string id)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Id == id);
+            var userCourant = _userManager.FindByIdAsync(_userManager.GetUserId(HttpContext.User)).Result;
+            var vm = new GestionComptesDeleteVM { Id = user.Id, Nom = user.UserName };
+            if (user == userCourant)
+            {
+                return PartialView("PartialViews/Modals/Comptes/_ErreurCompteUtilisePartial", vm);
+            }
+
+
+            return PartialView("PartialViews/Modals/Comptes/_DeleteMembrePartial", vm);
+        }
+        public IActionResult ShowDeleteConfirmationEmploye(string id)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Id == id);
+            var vm = new GestionComptesDeleteVM { Id = user.Id, Nom = user.UserName };
+
+            return PartialView("PartialViews/Modals/Comptes/_DeleteEmployePartial", vm);
+        }
+        public IActionResult ShowDeleteConfirmationAdmin(string id)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Id == id);
+            var vm = new GestionComptesDeleteVM { Id = user.Id, Nom = user.UserName };
+
+            return PartialView("PartialViews/Modals/Comptes/_DeleteAdminPartial", vm);
+        }
+        #endregion
+        [HttpPost]
+        public IActionResult SupprimerCompte(string id)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+                return NotFound();
+            else
+            {
+                //_context.Users.Remove(user);
+                _userManager.DeleteAsync(user);
+                _context.SaveChanges();
+                return Ok();
+            }
         }
     }
 }
