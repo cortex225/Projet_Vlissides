b57ea4cd09dccb6bfab32131da48e79f6ab6d1fa
TonyTheProg <tranloan946@gmail.com>
2023-10-25 16:52:00 -0400

    ajouter et supprimer fonctionnel

--- VLISSIDES/VLISSIDES/Controllers/GestionEvenementsController.cs
+++ VLISSIDES/VLISSIDES/Controllers/GestionEvenementsController.cs
@@ -1,15 +1,136 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using VLISSIDES.Data;
 using VLISSIDES.Models;
+using VLISSIDES.ViewModels.GestionEvenements;
 
 namespace VLISSIDES.Controllers
 {
     [Authorize(Roles = RoleName.EMPLOYE + ", " + RoleName.ADMIN)]
     public class GestionEvenementsController : Controller
     {
+        private readonly IConfiguration _config;
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public GestionEvenementsController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment,
+        IConfiguration config)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+            _config = config;
+        }
         public IActionResult Index()
         {
-            return View();
+            var evenements = _context.Evenements.Select(e => new GestionEvenementsIndexVM
+            {
+                Id = e.Id,
+                Nom = e.Nom,
+                Description = e.Description,
+                DateDebut = e.DateDebut,
+                DateFin = e.DateFin,
+                Image = e.Image,
+                Lieu = e.Lieu,
+                NbPlaces = e.NbPlaces,
+                NbPlacesMembre = e.NbPlacesMembre,
+                Prix = e.Prix,
+            }).ToList();
+            return View(evenements);
+        }
+        public IActionResult AfficherEvenements()
+        {
+            var evenements = _context.Evenements.Select(e => new GestionEvenementsIndexVM
+            {
+                Id = e.Id,
+                Nom = e.Nom,
+                Description = e.Description,
+                DateDebut = e.DateDebut,
+                DateFin = e.DateFin,
+                Image = e.Image,
+                Lieu = e.Lieu,
+                NbPlaces = e.NbPlaces,
+                NbPlacesMembre = e.NbPlacesMembre,
+                Prix = e.Prix,
+            }).ToList();
+            return PartialView("PartialViews/GestionEvenements/_ListeEvenementsPartial", evenements);
+        }
+        public IActionResult AjouterEvenement()
+        {
+            var vm = new GestionEvenementsAjouterVM();
+            return PartialView("PartialViews/Modals/Evenements/_AjouterEvenementsPartial", vm);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AjouterEvenement(GestionEvenementsAjouterVM vm)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var evenement = new Evenement()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Nom = vm.Nom,
+                    Description = vm.Description,
+                    DateDebut = vm.DateDebut,
+                    DateFin = vm.DateFin,
+                    Image = vm.Image,
+                    Lieu = vm.Lieu,
+                    NbPlaces = vm.NbPlaces,
+                    NbPlacesMembre = vm.NbPlacesMembre,
+                    Prix = vm.Prix,
+
+                };
+                if (vm.CoverPhoto != null)
+                {
+                    var wwwRootPath = _webHostEnvironment.WebRootPath;
+                    var fileName = Path.GetFileNameWithoutExtension(vm.CoverPhoto.FileName);
+                    var extension = Path.GetExtension(vm.CoverPhoto.FileName);
+                    fileName = fileName + "_" + Guid.NewGuid().ToString() +
+                               extension; // Utilisation de Guid pour un nom de fichier unique
+                    var folderPath =
+                        Path.Combine(wwwRootPath, "img",
+                            "EvenementImages"); // Chemin du dossier où l'image sera sauvegardée
+                    var fullPath = Path.Combine(folderPath, fileName); // Chemin complet du fichier
+
+                    // Sauvegarder l'image
+                    using (var fileStream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        await vm.CoverPhoto.CopyToAsync(fileStream);
+                    }
+
+                    evenement.Image =
+                        "/img/EvenementImages/" + fileName;
+                }
+                else
+                {
+                    evenement.Image = "/img/Couvertures/livredefault.png";
+                }
+                _context.Evenements.Add(evenement);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            return PartialView("PartialViews/Modals/Evenements/_AjouterEvenementsPartial", vm);
+        }
+
+        public IActionResult ShowSupprimerEvenement(string id)
+        {
+            var evenement = _context.Evenements.FirstOrDefault(e => e.Id == id);
+            var vm = new GestionEvenementSupprimerVM()
+            {
+                Id = evenement.Id,
+                Nom = evenement.Nom
+            };
+            return PartialView("PartialViews/Modals/Evenements/_SupprimerEvenementPartial", vm);
+        }
+        [HttpPost]
+        public IActionResult SupprimerEvenement(string id)
+        {
+            var evenement = _context.Evenements.FirstOrDefault(e => e.Id == id);
+            if (evenement == null)
+            {
+                return NotFound();
+            }
+            _context.Evenements.Remove(evenement);
+            _context.SaveChanges();
+            return Ok();
         }
     }
 }
---
+++ VLISSIDES/VLISSIDES/ViewModels/GestionEvenements/GestionEvenementsAjouterVM.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace VLISSIDES.ViewModels.GestionEvenements
+{
+    public class GestionEvenementsAjouterVM
+    {
+
+
+        [DisplayName("Nom")] public string Nom { get; set; } = default!;
+
+        [DisplayName("Description")] public string Description { get; set; } = default!;
+
+        [DisplayName("Image")] public string? Image { get; set; } = default!;
+
+        [DisplayName("Date de début")] public DateTime DateDebut { get; set; } = default!;
+
+        [DisplayName("Date de fin")] public DateTime DateFin { get; set; } = default!;
+
+        [DisplayName("Lieu")] public string Lieu { get; set; } = default!;
+
+        [DisplayName("Nombre de places total")]
+        public int NbPlaces { get; set; } = default!;
+
+        [DisplayName("Nombre de places réservé au membre")]
+        public int NbPlacesMembre { get; set; } = default!;
+
+        [DisplayName("Prix")] public decimal? Prix { get; set; } = default!;
+        [Display(Name = "Image du livre")] public IFormFile? CoverPhoto { get; set; }
+    }
+}
