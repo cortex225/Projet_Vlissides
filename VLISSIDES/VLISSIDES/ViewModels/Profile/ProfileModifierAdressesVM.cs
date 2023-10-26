3f79bac711c9577c7517db168c9e21da1f5f2a40
TonyTheProg <tranloan946@gmail.com>
2023-10-26 13:54:49 -0400

    profile

--- VLISSIDES/VLISSIDES/ViewModels/Profile/ProfileModifierAdressesVM.cs
+++ VLISSIDES/VLISSIDES/ViewModels/Profile/ProfileModifierAdressesVM.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using VLISSIDES.Models;
 
 namespace VLISSIDES.ViewModels.Profile
@@ -6,17 +7,19 @@ namespace VLISSIDES.ViewModels.Profile
     public class ProfileModifierAdressesVM
     {
         public string? Id { get; set; }
+        [Required(ErrorMessage = "Le numéro civique est obligatoire")]
         [DisplayName("Numéro civique")] public string NoCivique { get; set; } = default!;
-
+        [Required(ErrorMessage = "La rue est obligatoire")]
         [DisplayName("Rue")] public string Rue { get; set; } = default!;
+        [Required(ErrorMessage = "Le numéro apartement est obligatoire")]
         [DisplayName("Numéro apartement")] public string NoApartement { get; set; } = default!;
-
+        [Required(ErrorMessage = "La ville est obligatoire")]
         [DisplayName("Ville")] public string Ville { get; set; } = default!;
-
+        [Required(ErrorMessage = "La province est obligatoire")]
         [DisplayName("Province")] public string Province { get; set; } = default!;
-
+        [Required(ErrorMessage = "Le code postal est obligatoire")]
         [DisplayName("Code postal")] public string CodePostal { get; set; } = default!;
-
+        [Required(ErrorMessage = "Le pays est obligatoire")]
         [DisplayName("Pays")] public string Pays { get; set; } = default!;
         //public Adresse? AdressePrincipale { get; set; }
 
--- VLISSIDES/VLISSIDES/ViewModels/Profile/ProfileModifierInformationVM.cs
+++ VLISSIDES/VLISSIDES/ViewModels/Profile/ProfileModifierInformationVM.cs
@@ -5,14 +5,18 @@ namespace VLISSIDES.ViewModels.Profile
     public class ProfileModifierInformationVM
     {
         public string Id { get; set; }
+        [Required(ErrorMessage = "Le prénom est obligatoire")]
         [Display(Name = "Prénom")] public string Prenom { get; set; }
+        [Required(ErrorMessage = "Le nom est obligatoire")]
         public string Nom { get; set; }
-
+        [Required(ErrorMessage = "La date de naissance est obligatoire")]
         [Display(Name = "Date de naissance")] public DateTime? DateNaissance { get; set; }
+        [Required(ErrorMessage = "Le nom d'utilisateur est obligatoire")]
         [Display(Name = "Nom d'utilisateur")] public string NomUtilisateur { get; set; }
+        [Required(ErrorMessage = "Le courriel est obligatoire")]
         public string Courriel { get; set; }
 
-
+        [Required(ErrorMessage = "Le téléphone est obligatoire")]
         [Display(Name = "Téléphone")] public string Telephone { get; set; }
 
         public string? CoverImageUrl { get; set; }
