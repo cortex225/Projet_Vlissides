9cb596a7c5ccd608346986eac149b1383b95128e
BoomBoxProg <118985204+BoomBoxProg@users.noreply.github.com>
2023-10-26 13:43:53 -0400

    Filtrer et trier commandes

--- VLISSIDES/VLISSIDES/Controllers/GestionCommandesController.cs
+++ VLISSIDES/VLISSIDES/Controllers/GestionCommandesController.cs
@@ -25,8 +25,14 @@ namespace VLISSIDES.Controllers
             _userManager = userManager;
             _httpContextAccessor = httpContextAccessor;
         }
-        public IActionResult Index()
+        public IActionResult Index(string? motCles, string? criteres)
         {
+            //var listCriteresValue = new List<string>();
+            //if (motCles != null) listCriteresValue = motCles.Split('|').ToList();
+
+            //var listCriteres = new List<string>();
+            //if (criteres != null) listCriteres = criteres.Split('|').ToList();
+
             //var currentUserId = _userManager.GetUserId(HttpContext.User);
             var commandes = _context.Commandes
                 .Include(c => c.StatutCommande)
@@ -48,8 +54,9 @@ namespace VLISSIDES.Controllers
                 MembreUserName = c.Membre.UserName,
                 AdresseId = c.AdresseId,
                 LivreCommandes = livreCommandeVM.Where(lc => lc.CommandeId == c.Id).ToList(),
+                StatutId = c.StatutCommande.Id,
                 StatutNom = c.StatutCommande.Nom
-            }).ToList();
+            }).OrderBy(c => c.DateCommande).ToList();
 
             var affichageCommandes = new AffichageCommandeVM
             {
@@ -63,6 +70,73 @@ namespace VLISSIDES.Controllers
             }).ToList();
 
             return View(affichageCommandes);
+        }
+
+        public IActionResult AfficherCommandes(string? motCles, string? criteres)
+        {
+            var listCriteresValue = new List<string>();
+            if (motCles != null) listCriteresValue = motCles.Split('|').ToList();
+
+            var listCriteres = new List<string>();
+            if (criteres != null) listCriteres = criteres.Split('|').ToList();
+
+            //var currentUserId = _userManager.GetUserId(HttpContext.User);
+            var commandes = _context.Commandes
+                .Include(c => c.StatutCommande)
+                .Include(c => c.Membre);
+            var livreCommandes = _context.LivreCommandes;
+
+            var livreCommandeVM = livreCommandes.Select(lc => new LivreCommandeVM
+            {
+                Livre = lc.Livre,
+                CommandeId = lc.CommandeId,
+                Quantite = lc.Quantite
+            }).ToList();
+
+            var listeCommandeVM = commandes.AsEnumerable().Select(c => new CommandesVM
+            {
+                Id = c.Id.ToString(),
+                DateCommande = c.DateCommande,
+                PrixTotal = c.PrixTotal,
+                MembreUserName = c.Membre.UserName,
+                AdresseId = c.AdresseId,
+                LivreCommandes = livreCommandeVM.Where(lc => lc.CommandeId == c.Id).ToList(),
+                StatutId = c.StatutCommande.Id,
+                StatutNom = c.StatutCommande.Nom
+            }).OrderBy(c => c.DateCommande).ToList();
+
+            if (listCriteres.Any(c => c == "trierDate"))
+            {
+                if (listCriteresValue[0] == "2")
+                {
+                    listeCommandeVM = listeCommandeVM.OrderByDescending(c => c.DateCommande).ToList();
+                }
+                else
+                {
+                    listeCommandeVM = listeCommandeVM.OrderBy(c => c.DateCommande).ToList();
+                }
+            }
+
+            if (listCriteres.Any(c => c == "filtrerStatut"))
+            {
+                if (listCriteresValue[1] != "0")
+                {
+                    listeCommandeVM = listeCommandeVM.Where(c => c.StatutId == listCriteresValue[1].ToString()).ToList();
+                }
+            }
+
+            var affichageCommandes = new AffichageCommandeVM
+            {
+                ListCommandes = listeCommandeVM
+            };
+
+            affichageCommandes.SelectListStatut = _context.StatutCommandes.Select(s => new SelectListItem
+            {
+                Text = s.Nom,
+                Value = s.Id
+            }).ToList();
+
+            return PartialView("PartialViews/GestionCommandes/_ListeCommandesVM", affichageCommandes);
         }
     }
 }
