b8d4f17ae308fec81574a440f4c372c94b5492d6
TonyTheProg <tranloan946@gmail.com>
2023-10-30 11:14:17 -0400

    Liste de promotions

---
+++ VLISSIDES/VLISSIDES/ViewModels/GestionPromotions/GestionPromotionsIndexVM.cs
@@ -0,0 +1,22 @@
+﻿using VLISSIDES.Models;
+
+namespace VLISSIDES.ViewModels.GestionPromotions
+{
+    public class GestionPromotionsIndexVM
+    {
+
+        public string Id { get; set; }
+        public string Description { get; set; }
+
+        public decimal Rabais { get; set; }
+
+        public DateTime DateDebut { get; set; }
+
+        public DateTime DateFin { get; set; }
+
+
+
+        public ICollection<Livre>? Livres { get; set; }
+        public string? Image { get; set; }
+    }
+}
