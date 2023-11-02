99b601b149a00745ee3a13bdd7e9c0371515f722
jlgouaho <1900751@CegepGranby.qc.ca>
2023-11-01 23:57:02 -0400

    Envoie de courriel et mise à 0 du panier

--- VLISSIDES/VLISSIDES/API/Stripe/StripeController.cs
+++ VLISSIDES/VLISSIDES/API/Stripe/StripeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,8 @@ using VLISSIDES.ViewModels.Paiement;
 using VLISSIDES.ViewModels.Panier;
 using VLISSIDES.ViewModels.Profile;
 using Stripe.Checkout;
+using VLISSIDES.Interfaces;
+using VLISSIDES.ViewModels.GestionCommandes;
 
 namespace VLISSIDES.API.Stripe
 {
@@ -29,6 +32,8 @@ namespace VLISSIDES.API.Stripe
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IUserStore<ApplicationUser> _userStore;
         private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly ISendGridEmail _sendGridEmail;
+
 
         public StripeController(
             SignInManager<ApplicationUser> signInManager,
@@ -39,6 +44,7 @@ namespace VLISSIDES.API.Stripe
             ApplicationDbContext context,
             IHttpContextAccessor httpContextAccessor,
             IWebHostEnvironment webHostEnvironment,
+            ISendGridEmail sendGridEmail,
             IConfiguration config)
         {
             _signInManager = signInManager;
@@ -50,6 +56,7 @@ namespace VLISSIDES.API.Stripe
             _httpContextAccessor = httpContextAccessor;
             _webHostEnvironment = webHostEnvironment;
             _config = config;
+            _sendGridEmail = sendGridEmail;
         }
 
         [HttpPost("confirmation-paiement")]
@@ -73,33 +80,73 @@ namespace VLISSIDES.API.Stripe
                 }
                 else if (stripeEvent.Type == Events.CheckoutSessionCompleted)
                 {
-
-                    var customer =
-                        await _context.Membres.FirstOrDefaultAsync(c => c.StripeCustomerId == session.CustomerId);
-                    var panier = await _context.Paniers.FirstOrDefaultAsync(p => p.Id == customer.Id);
-                    var panierItems = await _context.Paniers.Where(pi => pi.Id == panier.Id).ToListAsync();
-                    var user = await _userManager.FindByIdAsync(customer.StripeCustomerId);
-                    var userClaims = await _userManager.GetClaimsAsync(user);
-                    var userRoles = await _userManager.GetRolesAsync(user);
-                    var userClaimsList = new List<string>();
-                    var userRolesList = new List<string>();
-                    foreach (var claim in userClaims)
+                    try
                     {
-                        userClaimsList.Add(claim.Value);
+                        var customer =
+                            await _context.Membres.FirstOrDefaultAsync(c => c.StripeCustomerId == session.CustomerId);
+                        var panierItems = _context.LivrePanier
+                            .Where(lp => lp.UserId == customer.Id)
+                            .Include(lp => lp.Livre).ThenInclude(livre => livre.LivreTypeLivres)
+                            .ToList();
+
+
+                        var nouvelleCommande = new CommandesVM();
+                        nouvelleCommande.DateCommande = DateTime.Now;
+                        nouvelleCommande.PrixTotal = session.AmountTotal.Value/100m;
+                        nouvelleCommande.MembreUserName = customer.UserName;
+                        nouvelleCommande.AdresseId = customer?.AdressePrincipaleId;
+                        nouvelleCommande.StatutId = "1";
+                        nouvelleCommande.LivreCommandes = panierItems.Select(lc => new LivreCommandeVM
+                        {
+                            Livre = lc.Livre,
+                            CommandeId = nouvelleCommande.Id,
+                            Quantite = (int)lc.Quantite
+                        }).ToList();
+
+                        var nbCommandes = _context.Commandes.Count().ToString();
+                        var commande = new Commande
+                        {
+                            Id = "Commande-"+nbCommandes+"-"+ DateTime.Now.ToString("yyyyMMddHH"),
+                            DateCommande = nouvelleCommande.DateCommande,
+                            PrixTotal = nouvelleCommande.PrixTotal,
+                            MembreId = customer.Id,
+                            AdresseId = customer.AdressePrincipaleId,
+                            StatutCommandeId = nouvelleCommande.StatutId,
+                            LivreCommandes = nouvelleCommande.LivreCommandes.Select(lc => new LivreCommande
+                            {
+                                LivreId = lc.Livre.Id,
+                                CommandeId = nouvelleCommande.Id,
+                                Quantite = lc.Quantite
+                            }).ToList()
+
+                        };
+
+                         _context.Commandes.Add(commande);
+                         await _context.SaveChangesAsync();
+
+                        // Récupérer l'URL complète du logo à partir de l'application
+                         var logoUrl =
+                             Url.Content(
+                                 "http://ivoxcommunication.com/v2/wp-content/uploads/2023/09/Logo_sans_fond.png");
+                         // Envoi du mail de confirmation de commande
+                            await SendConfirmationEmail(customer, nouvelleCommande, logoUrl, session.Id);
+
+                        //Suppression du panier actuel de la bd
+                        _context.LivrePanier.RemoveRange(panierItems);
+                        await _context.SaveChangesAsync();
+
+
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        throw;
                     }
 
-                    foreach (var role in userRoles)
-                    {
-                        userRolesList.Add(role);
-                    }
 
                 }
-                else if (stripeEvent.Type == Events.CheckoutSessionExpired)
-                {
-                }
-                else if (stripeEvent.Type == Events.PaymentIntentSucceeded)
-                {
-                }
+              
                 else
                 {
                     // Unexpected event type
@@ -112,6 +159,64 @@ namespace VLISSIDES.API.Stripe
             {
                 return BadRequest();
             }
+        }
+
+        private async Task SendConfirmationEmail(Membre customer, CommandesVM nouvelleCommande, string logoUrl,
+            string sessionId)
+        {
+            var subject = "Confirmation de commande";
+
+            // Récupérer la facture de Stripe
+            // var invoice = await GetInvoiceFromStripe(sessionId);
+
+            // Construire le corps du courriel
+            var body = BuildEmailBody(customer, nouvelleCommande, logoUrl);
+
+            // Envoyer le courriel avec la facture en pièce jointe
+            await _sendGridEmail.SendEmailAsync(customer.Email, subject, body);
+        }
+
+
+
+
+        private string BuildEmailBody(Membre customer, CommandesVM nouvelleCommande, string logoUrl)
+        {
+            StringBuilder body = new StringBuilder();
+
+            body.Append("<div style='font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto;'>");
+            body.Append(
+                $"<img src='{logoUrl}' alt='Logo Fourmie Aillée' style='width:250px;height:auto; display: block; margin: 0 auto;'>");
+            body.Append($"<h2 style='color: #444;'>Cher(e) {customer.UserName},</h2>");
+            body.Append("<p>Merci pour votre commande ! Voici le récapitulatif :</p>");
+            body.Append("<table style='width: 100%; border-collapse: collapse;'>");
+            body.Append("<thead>");
+            body.Append("<tr style='background-color: #f2f2f2;'>");
+            body.Append("<th style='padding: 8px; border: 1px solid #ddd;'>Produit</th>");
+            body.Append("<th style='padding: 8px; border: 1px solid #ddd;'>Quantité</th>");
+            body.Append("<th style='padding: 8px; border: 1px solid #ddd;'>Prix</th>");
+            body.Append("</tr>");
+            body.Append("</thead>");
+            body.Append("<tbody>");
+
+            foreach (var item in nouvelleCommande.LivreCommandes)
+            {
+
+                body.Append("<tr>");
+                body.Append($"<td style='padding: 8px; border: 1px solid #ddd;'>{item.Livre.Titre}</td>");
+                body.Append($"<td style='padding: 8px; border: 1px solid #ddd;'>{item.Quantite}</td>");
+                body.Append($"<td style='padding: 8px; border: 1px solid #ddd;'>{item.Livre.LivreTypeLivres.FirstOrDefault().Prix}$</td>");
+                body.Append("</tr>");
+            }
+
+            body.Append("</tbody>");
+            body.Append("</table>");
+            body.Append($"<p><strong>Total : {nouvelleCommande.PrixTotal.ToString("C")}</strong></p>");
+            body.Append(
+                "<p>Votre commande sera traitée rapidement et vous recevrez une notification dès qu'elle sera expédiée.</p>");
+            body.Append("<p>Merci de faire confiance à La Fourmie Aillée !</p>");
+            body.Append("</div>");
+
+            return body.ToString();
         }
     }
 }
