bcc7a7eb2f600dea51f39c94eb2162296a3a47a2
jlgouaho <1900751@CegepGranby.qc.ca>
2023-11-06 10:47:41 -0500

    Update SendGridEmailAdvance.cs

--- VLISSIDES/VLISSIDES/Services/SendGridEmailAdvance.cs
+++ VLISSIDES/VLISSIDES/Services/SendGridEmailAdvance.cs
@@ -8,33 +8,44 @@ namespace VLISSIDES.Services;
 
 public class SendGridEmailAdvance : ISendGridEmailAdvance
 {
+    // Déclaration d'une variable pour enregistrer les messages d'erreur ou d'information.
     private readonly ILogger<SendGridEmailAdvance> _logger;
+
+    // Propriété publique pour accéder aux options de configuration de SendGrid.
     public AuthMessageSenderOptions Options { get; set; }
 
+    // Constructeur de la classe qui initialise les options de configuration et le logger.
     public SendGridEmailAdvance(IOptions<AuthMessageSenderOptions> optionsAccessor, ILogger<SendGridEmailAdvance> logger)
     {
         Options = optionsAccessor.Value;
         _logger = logger;
     }
 
-    // Méthode pour envoyer un e-mail en texte brut
+    // Méthode asynchrone pour envoyer un e-mail en texte brut.
     public async Task SendEmailAsync(string toEmail, string subject, string message)
     {
+        // Vérifie si la clé API de SendGrid est présente, sinon lance une exception.
         if (string.IsNullOrEmpty(Options.ApiKey)) throw new Exception("Null SendGridKey");
+        // Appelle la méthode Execute pour envoyer l'email.
         await Execute(Options.ApiKey, subject, message, toEmail, null, false);
     }
 
-    // Méthode pour envoyer un e-mail HTML avec des pièces jointes
+    // Méthode asynchrone pour envoyer un e-mail HTML avec des pièces jointes.
     public async Task SendEmailAsync(string toEmail, string subject, string content, bool isHtml,
         List<Attachment>? attachments)
     {
+        // Vérifie si la clé API de SendGrid est présente, sinon lance une exception.
         if (string.IsNullOrEmpty(Options.ApiKey)) throw new Exception("Null SendGridKey");
+        // Appelle la méthode Execute pour envoyer l'email.
         await Execute(Options.ApiKey, subject, content, toEmail, attachments, isHtml);
     }
 
+    // Méthode privée asynchrone qui effectue l'envoi de l'e-mail.
     private async Task Execute(string apiKey, string subject, string content, string toEmail, List<Attachment> attachments, bool isHtml)
     {
+        // Crée un nouveau client SendGrid avec la clé API.
         var client = new SendGridClient(apiKey);
+        // Crée un nouveau message SendGrid.
         var msg = new SendGridMessage
         {
             From = new EmailAddress("1900751@cegepgranby.qc.ca", "La Fourmie Aillée"),
@@ -43,8 +54,10 @@ public class SendGridEmailAdvance : ISendGridEmailAdvance
             HtmlContent = isHtml ? content : null
         };
 
+        // Ajoute le destinataire de l'e-mail.
         msg.AddTo(new EmailAddress(toEmail));
 
+        // Si des pièces jointes sont fournies, les ajoute au message.
         if (attachments != null)
         {
             foreach (var attachment in attachments)
@@ -53,10 +66,13 @@ public class SendGridEmailAdvance : ISendGridEmailAdvance
             }
         }
 
+        // Désactive le suivi des clics dans l'e-mail.
         msg.SetClickTracking(false, false);
 
+        // Envoie l'e-mail et attend la réponse de SendGrid.
         var response = await client.SendEmailAsync(msg);
 
+        // Enregistre un message d'information dans le logger selon le succès ou l'échec de l'envoi.
         _logger.LogInformation(response.IsSuccessStatusCode
             ? $"Email to {toEmail} queued successfully!"
             : $"Failure Email to {toEmail}");
