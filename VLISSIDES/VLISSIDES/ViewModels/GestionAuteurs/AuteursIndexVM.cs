5889ece1a52d35795442fc4e4e7178e6725ac910
David-Messier <99209339+David-Messier@users.noreply.github.com>
2023-11-27 00:15:54 -0500

    Testé

--- VLISSIDES/VLISSIDES/ViewModels/GestionAuteurs/AuteursIndexVM.cs
+++ VLISSIDES/VLISSIDES/ViewModels/GestionAuteurs/AuteursIndexVM.cs
@@ -6,4 +6,10 @@ public class AuteursIndexVM
 {
     [DisplayName("Auteurs")] public List<AuteursAfficherVM>? Auteurs { get; set; }
     [DisplayName("Auteur ajouter")] public AuteursAjouterVM? AuteurAjouter { get; set; }
+
+    public AuteursIndexVM(List<AuteursAfficherVM>? auteurs, AuteursAjouterVM? auteurAjouter)
+    {
+        Auteurs = auteurs;
+        AuteurAjouter = auteurAjouter;
+    }
 }
