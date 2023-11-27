37e32ef3b59c2ebb4199e6bf78a1e411956331e8
TonyTheProg <tranloan946@gmail.com>
2023-11-27 10:03:59 -0500

    Telephone

--- VLISSIDES/VLISSIDES/Controllers/ProfileController.cs
+++ VLISSIDES/VLISSIDES/Controllers/ProfileController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 using VLISSIDES.Data;
 using VLISSIDES.Models;
 using VLISSIDES.ViewModels.Profile;
@@ -88,6 +89,11 @@ public class ProfileController : Controller
 
         if (ModelState.IsValid)
         {
+            if (!Regex.Match(vm.Telephone, "^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$").Success)
+            {
+                ModelState.AddModelError("Telephone", "Le numéro de téléphone doit avoir le format : 123-456-7890");
+                return View("Index", indexVM);
+            }
             var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == vm.Id);
             var userM = await _userManager.Users.FirstOrDefaultAsync(u => u.Id == vm.Id);
 
