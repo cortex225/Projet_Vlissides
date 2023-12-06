5d70fd2b463f87e35fed335a5f4b30935b541ae9
TonyTheProg <tranloan946@gmail.com>
2023-12-06 17:27:06 -0500

    stripe customer id

--- VLISSIDES/Seeder/DbContextFactory.cs
+++ VLISSIDES/Seeder/DbContextFactory.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Stripe;
 using VLISSIDES.Data;
 
 namespace Seeder;
@@ -8,6 +9,7 @@ public class DbContextFactory
 {
     public static ApplicationDbContext CreateDbContext()
     {
+
         var configuration = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile(Directory.GetCurrentDirectory() + "/appsettings.json")
@@ -21,7 +23,7 @@ public class DbContextFactory
 
 
         var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
-
+        StripeConfiguration.ApiKey = "sk_test_51O3gRDAIrJUcHyNnZxFOOXj7BU4fydHMbsMvSGoA33X1DrKBSBnDswvi5cqwoCLEGmd5WCPOEc5bUORrTRM0U7Vt00yyd5Gmyk";
 
         if (OperatingSystem.IsMacOS()) builder.UseSqlServer(configuration.GetConnectionString("JLConnection"));
         else builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
