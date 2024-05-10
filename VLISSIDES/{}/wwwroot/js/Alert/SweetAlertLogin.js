51570a6fbb6124f330946b7f04223ca4f42139bb
jlgouaho <1900751@CegepGranby.qc.ca>
2024-05-10 00:19:24 -0400

    great

---
+++ VLISSIDES/{}/wwwroot/js/Alert/SweetAlertLogin.js
@@ -0,0 +1,29 @@
+// SweetAlert2 pour afficher une notification de succès ou d 'erreur
+
+// Attache un gestionnaire d'événements de soumission à tous les formulaires sur la page
+$('form').submit(function (e) {
+  e.preventDefault() // Empêche la soumission par défaut du formulaire
+  $.ajax({
+    // Obtient l'URL d'action du formulaire
+    url: $(this).attr('action'),
+    type: 'POST',
+    data: $(this).serialize(), // Sérialise les données du formulaire pour les envoyer dans la requête
+    success: function (result) {
+      Swal.fire({
+        icon: 'success',
+        title: result,
+        showConfirmButton: false,
+        timer: 3000
+      }).then((result) => {
+        window.location.href = '@Url.Action("Login", "Compte")'
+      })
+    },
+    error: function (xhr, status, error) {
+      Swal.fire({
+        icon: 'error',
+        title: "Une erreur s'est produite",
+        text: xhr.responseText
+      })
+    }
+  })
+})
---
+++ VLISSIDES/{}/wwwroot/js/InventaireModifierQuantite.js
@@ -0,0 +1,39 @@
+function saveQuantite (id = '') {
+  fetch(
+    `ModifierLivreQuantite?id=${id}&quantite=${document.getElementById(id + ' quantite').value}`,
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+  ).catch((error) =>
+    console.error('Échec du fetch pour modifier la quantité de livre', error)
+  )
+}
+
+function SupprimerLivre (id = '') {
+  fetch(`Delete?id=${id}`, {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+    .catch((error) => {
+      console.error('Échec du fetch pour supprimer le livre', error)
+    })
+    .then(() => {
+      document.getElementById(`${id}`).remove()
+      $('#ModalId').modal('hide') // Ferme la modale
+    })
+}
+
+// ModalScript
+function ShowModal ($url) {
+  $.get($url, function (result) {
+    $('.modal-content').html(result)
+    $('.modal').modal('show')
+  })
+}
